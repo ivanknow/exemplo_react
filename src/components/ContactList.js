d398de94c2756514671f5face4b8e3ef1d51e3ec
Ivan Rodrigues <ivanknow@gmail.com>
2021-10-06 00:16:30 -0300

    layout

---
+++ src/components/ContactList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const  ContactList = () => {
+    return (
+      <div className="ui celled list"> Contac List </div>
+    );
+  }
+  
+  export default ContactList;
+  
